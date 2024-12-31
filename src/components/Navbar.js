deea1be4c677096eb521573191d9e71ca055389d
Ranimmmmm <rmouadb66@gmail.com>
2024-12-31 14:27:27 +0100

    front added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,31 +7,6 @@ const Navbar = ({ userProfile }) => {
     <nav className="navbar navbar-expand-lg fixed-top navbar-scroll">
       <div className="container-fluid">
         <h2>Activité d'équipe</h2>
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a href="#notifications" className="nav-link">
-              <i className="fas fa-bell"></i>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#user-profile" className="nav-link">
-              {userProfile ? (
-                <>
-                  <img
-                    src={userProfile?.picture}
-                    alt="***"
-                    className="rounded-circle"
-                    width="32"
-                    height="32"
-                  />
-                  <span className="ms-2">{userProfile.name}</span>
-                </>
-              ) : (
-                <span>Loading...</span>
-              )}
-            </a>
-          </li>
-        </ul>
       </div>
     </nav>
   );
